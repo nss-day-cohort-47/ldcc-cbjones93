49f9911e540ecbb3d5bce72ae404cfa250d22719
Cody_Jones <cbjones93@gmail.com>
2021-03-26 13:47:37 -0500

    details now displayed properly

--- scripts/data/apiManager.js
+++ scripts/data/apiManager.js
@@ -1,3 +1,4 @@
+
 const apiURL = "http://localhost:8088";
 
 //// user functions
@@ -68,6 +69,6 @@ export const getSnacks = () => {
 }
 
 export const getSingleSnack = (snackId) => {
-	return fetch(`${apiURL}/snacks/${snackId}`)
+	return fetch(`${apiURL}/snacks/${snackId}?_expand=type&_expand=shape&_expand=inFlavor&_expand=season`)
 	.then(response => response.json())
 }
